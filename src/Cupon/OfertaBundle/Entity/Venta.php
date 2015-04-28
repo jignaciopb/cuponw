5ea63483099e5b5a43a885d87e9852c5b191f7d6
jignaciopb <jignaciopb@gmail.com>
2015-04-28 19:42:53 +0200

    base de datos

---
+++ src/Cupon/OfertaBundle/Entity/Venta.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Cupon\OfertaBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+
+class Venta {
+
+    /** @ORM\Column(type="datetime") */
+    protected $fecha;
+
+    /** @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Cupon\OfertaBundle\Entity\Oferta")
+     */
+    protected $oferta;
+
+    /**
+     * @ORM\Id
+     * @ORM\ManytoOne(targetEntity="Cupon\UsuarioBundle\Entity\Usuario")
+     */
+    protected $usuario;
+
+    public function setFecha()
+    {
+        $this->fecha = $fecha;
+    }
+
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    public function setOferta(\Cupon\OfertaBundle\Entity\Oferta $oferta)
+    {
+        $this->oferta = $oferta;
+    }
+
+    public function getOferta()
+    {
+        return $this->oferta;
+    }
+
+    public function setUsuario(\Cupon\UsuarioBundle\Entity\Usuario $usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+
+}
